cc3a3c9350d168432142a2e73fd357494368345b
chwnk <48645500+chwnk@users.noreply.github.com>
2021-02-27 00:44:37 +0100

    Finished
    Finished

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary;
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var directory = Directory.GetCurrentDirectory();
+            var issuesDir = new FileInfo(directory).Directory.Parent.Parent.FullName;
+            var resultDir = Directory.CreateDirectory(Path.Combine(directory, "result"));
+            OJSMigration.Migrate(Path.Combine(issuesDir, "issues.xml"), resultDir.FullName);
+        }
+    }
+}
